0407031d01b08e371a296c466dc1584a3d7dc980
Petrova <879662@fontys.nl>
2018-10-19 18:24:55 +0200

    moving objects

---
+++ Shooter/Assets/Scripts/TargetDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDisplay : MonoBehaviour {
+
+    [SerializeField]
+    Target[] targets;
+    // Use this for initialization
+    void Start () {
+        targets[0].gameObject.SetActive(false);
+        targets[1].gameObject.SetActive(false);
+        targets[2].gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
